585f131983d31056f89278792c8241f5d89c0644
Birthman <84112536+Birthman@users.noreply.github.com>
2025-06-01 06:18:13 -0700

    Delete magic_website directory

--- magic_website/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.routes.wish import wish_bp
-
-app = Flask(__name__, template_folder="templates")
-
-app.register_blueprint(wish_bp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
